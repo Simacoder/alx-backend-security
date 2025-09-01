535bde3201dda940f1b45e2ef53569ba304939f2
Simanga <simacoder@hotmail.com>
2025-09-01 13:11:11 +0200

    lapho sikhona

--- ip_tracking/ip_tracking/settings.py
+++ ip_tracking/ip_tracking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,6 +107,15 @@ CACHES = {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
         "LOCATION": "rate-limit-cache",
     }
+}
+
+
+
+CELERY_BEAT_SCHEDULE = {
+    "detect-anomalies-hourly": {
+        "task": "ip_tracking.tasks.detect_anomalies",
+        "schedule": crontab(minute=0, hour="*"),  # every hour
+    },
 }
 
 
